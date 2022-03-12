7e6561766261f9269cebf3d49306cd776fa1410f
Thales <thales.y@gmail.com>
2022-03-12 15:49:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import GlobalStyle from "./styles/global"
+
+function App() {
+  return (
+    <>
+      <h2>Testando</h2>
+
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
